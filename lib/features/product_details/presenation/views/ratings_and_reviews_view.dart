9c30a34e5fdfa78e4856e9c49737c16c5e4f244c
Mostafa Mohd <mostafa_9bbr1@yahoo.com>
2024-06-04 11:32:21 +0300

    Refactor RatingsAndReviewsView widget
    - Refactored the RatingsAndReviewsView widget to improve its readability and maintainability.
    - Added a bottom sheet with a transparent background to improve the user experience.
    - Reorganized the layout of the widget to make it more intuitive and user-friendly.
    - Added a checkbox to show photos in reviews.
    - Updated the styling of the widget to improve its visual appeal.

--- lib/features/product_details/presenation/views/ratings_and_reviews_view.dart
+++ lib/features/product_details/presenation/views/ratings_and_reviews_view.dart
@@ -6,99 +6,119 @@ import 'package:e_commerce_app/features/product_details/presenation/widgets/writ
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
-
 class RatingsAndReviewsView extends StatelessWidget {
   const RatingsAndReviewsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text("Ratings and Reviews"),
+    return Theme(
+      data: Theme.of(context).copyWith(
+        bottomSheetTheme: const BottomSheetThemeData(
+          backgroundColor: Colors.transparent,
+          shape: RoundedRectangleBorder(),
+        ),
       ),
-      floatingActionButton: const WriteAReviewBtn(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 12,
-          ),
-          child: Column(
-            children: [
-              SizedBox(
-                height: context.height * .15,
-                child: const Row(
+      child: Scaffold(
+        bottomSheet: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.white.withOpacity(.2),
+              Colors.white,
+            ],
+          )),
+          height: 100,
+          width: context.width,
+        ),
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("Ratings and Reviews"),
+        ),
+        floatingActionButton: const WriteAReviewBtn(),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 12,
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: context.height * .15,
+                  child: const Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "4.5",
+                            style: TextStyle(
+                              fontSize: 44,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Text(
+                            "20 ratings",
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey,
+                            ),
+                          )
+                        ],
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: [
+                            RatingsBuilder(
+                              rating: 5,
+                              number: 12,
+                            ),
+                            RatingsBuilder(
+                              rating: 4,
+                              number: 10,
+                            ),
+                            RatingsBuilder(
+                              rating: 3,
+                              number: 4,
+                            ),
+                            RatingsBuilder(
+                              rating: 2,
+                              number: 2,
+                            ),
+                            RatingsBuilder(
+                              rating: 1,
+                              number: 0,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Gap(10),
+                const Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    Row(
                       children: [
                         Text(
-                          "4.5",
+                          '8 Reviews',
                           style: TextStyle(
-                            fontSize: 44,
+                            fontSize: 24,
                             fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        Text(
-                          "20 ratings",
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.grey,
                           ),
                         )
                       ],
                     ),
-                    Expanded(
-                      child: Column(
-                        children: [
-                          RatingsBuilder(
-                            rating: 5,
-                            number: 12,
-                          ),
-                          RatingsBuilder(
-                            rating: 4,
-                            number: 10,
-                          ),
-                          RatingsBuilder(
-                            rating: 3,
-                            number: 4,
-                          ),
-                          RatingsBuilder(
-                            rating: 2,
-                            number: 2,
-                          ),
-                          RatingsBuilder(
-                            rating: 1,
-                            number: 0,
-                          ),
-                        ],
-                      ),
-                    ),
+                    ShowPhotoInReviewsCheckBox(),
                   ],
                 ),
-              ),
-              const Gap(10),
-              const Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        '8 Reviews',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      )
-                    ],
-                  ),
-                  ShowPhotoInReviewsCheckBox(),
-                ],
-              ),
-              const ReviewsListViewWidget(),
-            ],
+                const ReviewsListViewWidget(),
+              ],
+            ),
           ),
         ),
       ),
